5d87a0eaf2f81ba319fff273e7fdeda1d83fa427
Abhishek-raj093 <ss0986825@gmail.com>
2025-04-23 17:45:20 +0530

    UI Complete

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ImageUpload from './ImageUpload'
 import ImagePreview from './ImagePreview'
 
 const Home = () => {
+    const [uploadImage, setUploadImage] = useState(null);
+    const [enhancedImage, setEnhancedImage] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    const UploadImageHandler = async (file) => {
+        setUploadImage(URL.createObjectURL(file));
+        setLoading(true);
+
+        // Call the API to enhance the image
+        try {
+            // code which may produce error
+        } catch (error) {
+            // Code to handle the Error and Show Message
+        }
+    };
+
   return (
-    <div className=''>
-      <ImageUpload/>
-      <ImagePreview/>
-    </div>
+    <>
+      <ImageUpload UploadImageHandler={UploadImageHandler}/>
+      <ImagePreview 
+        loading={loading}
+        uploaded={uploadImage}
+        enhanced={enhancedImage}
+      />
+    </>
   )
 }
 
