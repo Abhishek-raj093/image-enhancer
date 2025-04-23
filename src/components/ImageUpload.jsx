e16d5d94e8548d110a985cd2ad457b71eabbf7ac
Abhishek-raj093 <ss0986825@gmail.com>
2025-04-23 07:45:36 +0530

    FIrst Commit

---
+++ src/components/ImageUpload.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ImageUpload = () => {
+  return (
+    <div className='bg-white shadow-lg rounded-2xl p-6 w-full max-w-2xl'>
+      ImageUpload
+    </div>
+  )
+}
+
+export default ImageUpload
